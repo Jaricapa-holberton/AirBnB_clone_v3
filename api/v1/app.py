25e37bb710704157bc06de39393090c80fa0cbf0
Jaricapa-holberton <2241@holbertonschool.com>
2021-09-18 16:09:08 -0500

    create task 13 v1

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,13 +2,15 @@
 """
 starts a Flask web application
 """
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 """@app.route('/', strict_slashes=False)
 def index():
